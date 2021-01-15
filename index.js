ad2d22b6e5885103336f3590738313c37ce3a1d0
Drothschild89 <Drothschild89@gmail.com>
2021-01-15 10:39:00 -0800

    commit tree f97f5506595db3d49c938816125f5635fe208302

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1
+let counter = 0;
+let mod = counter %=6;
+
+
+let max = Math.max(0,20)
